64aec841482ad90c2e13eb30d25f3a854e6ca5e4
lastuniverse <lastuniverse@mail.ru>
2020-10-12 01:17:28 +0200

    autopush: test .gitignore

--- modules/__trash/old.cluster.v01/index.js
+++
@@ -1,7 +0,0 @@
-
-
-exports = module.exports = {
-	Cluster: require('./source/cluster.js'),
-	Worker: require('./source/worker.js'),
-	Process: require('./source/process.js')
-};
--- modules/__trash/old.network.v01/index.js
+++
@@ -1,3 +0,0 @@
-
-
-exports = module.exports = require('./source/network.js');
