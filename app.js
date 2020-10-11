890be7f86bc3c2d26d7573c9ea181f610dee2e0d
lastuniverse <lastuniverse@mail.ru>
2020-10-12 00:44:34 +0200

    autopush: add vs code as editor, add ESLint, cosmetic edit for ESlint with airbnb

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-console */
 const path = require('path');
 
-const fs = require("fs");
+const fs = require('fs');
 
 const Cluster = require('./modules/cluster');
-
-const Storage = require('./modules/storage');
 
 const startWebApp = require('./modules/tools/webapp.js');
 
@@ -13,296 +13,285 @@ const config = require('./config.json');
 // создаем кластер
 const cluster = new Cluster();
 
-
-
 // использовал для логирования, но предназначенаа для других целей
 // const storage = Storage(path.join(__dirname, "/logs"));
 // создаем хранилище для автозаписи логовж
 // const log = storage.create(process.pid+'.log', {
-// 	generated: {}, // сюда заносим pid-ы процессов, и отправленные&&обработтанныt данные для каждого из pid
-// 	processed: [], // сюда заносим обработтанные данные в этом процессе
+//   generated: {}, // сюда заносим pid-ы процессов, и отправленные
+//                  // &&обработтанныt данные для каждого из pid
+//   processed: [], // сюда заносим обработтанные данные в этом процессе
 // });
 
 // логирую просто в файлы с именем {pid}.log в папке ./logs
-const logFfileName = path.join(__dirname, "/logs", process.pid+".log");
+const logFfileName = path.join(__dirname, '/logs', `${process.pid}.log`);
 const fd = fs.openSync(logFfileName, 'a');
 
 // это вместо редиса)) пересылается при смене мастера, отправляется в интерфейс через ws
 let workers = {};
 let info = {
-	generated: 0,
-	processed: 0,
+  generated: 0,
+  processed: 0,
 };
-
-// помогает запускать и останавливать генерацию данных для обработки (начиная с 257 строки и до конца генерация и обработка данных)
-class DataProcess{
-	constructor(){
-		this.isStarted = false;
-		this.timers = [];
-	}
-	start(cb,timeout=0){
-		const timer = setInterval(cb, timeout);
-		this.timers.push(timer);
-	}
-	stop(){
-		this.timers.forEach((timer)=>{
-			clearInterval(timer);
-		});
-		this.timers=[];	
-	}
-};
-const dataProcess = new DataProcess(1000);
-
-
-
-
-if (cluster.isMaster) {
-	// данный блок кода будет выполнен единожды, в мастер процессе при первом его запуске
-	// при передачи прав мастерпроцесса воркерам, данный блок в воркере выполнен не будет
-	start(cluster);
-
-	// 	запускаем воркеры (количество указанно в конфиге ./config.json)
-	for (var i = 0; i < config.workers; i++) {
-		cluster.fork();
-	}
-
-	// ожидаем от кластера подтверждения о готовности (запущен ipc сервер на мастере, все воркеры, запускаемые в этой секции готовы, 
-	// запустили свои IPC сервера, подключились к мастерпроцессу, мастер процесс подключился к каждому из воркеров)
-	cluster.on('cluster.ready', (pid) => {
-		// производим первичную инициализацию клобального ассоциативного массива workers
-		cluster.getWorkers().forEach((worker) => {
-			storeWorker(worker.pid);
-		});
-	});
-
-} else {
-	// данный блок кода будет выполнен единожды, в в каждом запускаемом точернем процессе
-
-	cluster.on('cluster.setmaster', (pid) => {});
-
-	cluster.on('cluster.ready', (pid) => {});
-
-	// перед тем как передать мастера, сервер кидает инфу
-	cluster.on("task.fullinfo", (w, i, pid) => {
-		workers = w;
-		info = i;
-		cluster.send(pid, "task.isReady", true);
-	});
-
-	// получаем мастера
-	cluster.on('cluster.isMaster', (from) => {
-		start(cluster);
-		cluster.send(from, "task.isMaster", true);
-		ws.broadcast("worker.list", workers);
-		// пишем в лог
-		fs.write(fd, `set as master from ${from}\n`,()=>{});
-	});
-
-}
-
 
 function storeWorker(pid, opt = {}) {
-	if (!pid) return;
-	const worker = cluster.getWorker(pid);
-	const current = workers[pid] || {};
-	const alive = (typeof opt.alive == 'boolean') ? opt.alive : worker ? true : false;
-	workers[pid] = {
-		pid: pid,
-		generated: opt.generated || current.generated || 0,
-		processed: opt.processed || current.processed || 0,
-		status: opt.status ? opt.status : worker.isMaster ? 'master' : 'worker',
+  if (!pid) return;
+  const worker = cluster.getWorker(pid);
+  const current = workers[pid] || {};
+  const alive = (typeof opt.alive === 'boolean') ? opt.alive : !!worker;
+  workers[pid] = {
+    pid,
+    generated: opt.generated || current.generated || 0,
+    processed: opt.processed || current.processed || 0,
+    // eslint-disable-next-line no-nested-ternary
+    status: opt.status ? opt.status : worker.isMaster ? 'master' : 'worker',
 
-		alive: alive
-	};
+    alive,
+  };
+}
+
+// помогает запускать и останавливать генерацию данных для обработки
+// (начиная с 257 строки и до конца генерация и обработка данных)
+class DataProcess {
+  constructor() {
+    this.isStarted = false;
+    this.timers = [];
+  }
+
+  start(cb, timeout = 0) {
+    const timer = setInterval(cb, timeout);
+    this.timers.push(timer);
+  }
+
+  stop() {
+    this.timers.forEach((timer) => {
+      clearInterval(timer);
+    });
+    this.timers = [];
+  }
+}
+const dataProcess = new DataProcess(1000);
+
+if (cluster.isMaster) {
+  // данный блок кода будет выполнен единожды, в мастер процессе при первом его запуске
+  // при передачи прав мастерпроцесса воркерам, данный блок в воркере выполнен не будет
+  start(cluster);
+
+  // запускаем воркеры (количество указанно в конфиге ./config.json)
+  for (let i = 0; i < config.workers; i++) {
+    cluster.fork();
+  }
+
+  // ожидаем от кластера подтверждения о готовности (запущен ipc сервер
+  // на мастере, все воркеры, запускаемые в этой секции готовы,
+  // запустили свои IPC сервера, подключились к мастерпроцессу,
+  // мастер процесс подключился к каждому из воркеров)
+  cluster.on('cluster.ready', (pid) => {
+    // производим первичную инициализацию клобального ассоциативного массива workers
+    cluster.getWorkers().forEach((worker) => {
+      storeWorker(worker.pid);
+    });
+  });
+} else {
+  // данный блок кода будет выполнен единожды, в в каждом запускаемом точернем процессе
+
+  cluster.on('cluster.setmaster', (pid) => {});
+
+  cluster.on('cluster.ready', (pid) => {});
+
+  // перед тем как передать мастера, сервер кидает инфу
+  cluster.on('task.fullinfo', (w, i, pid) => {
+    workers = w;
+    info = i;
+    cluster.send(pid, 'task.isReady', true);
+  });
+
+  // получаем мастера
+  cluster.on('cluster.isMaster', (from) => {
+    start(cluster);
+    cluster.send(from, 'task.isMaster', true);
+    // пишем в лог
+    fs.write(fd, `set as master from ${from}\n`, () => {});
+  });
 }
 
 // эта функция вызывается процессом, который становится мастером
 // - запускает express в связке websocker сервером
 // - обрабатывает события от интерфейса
 // - запускает генерацию и рассылку рандомных данных
-// 
+//
 function start(cluster) {
+  const ws = startWebApp(path.join(__dirname, 'public'), config.port);
 
-	
-	ws = startWebApp(path.join(__dirname, 'public'), config.port);
+  // далее в основном обработка событий, названия которых говорят сами за себя
+  cluster.on('cluster.ready', (pid) => {
+    ws.broadcast('worker.list', workers);
+  });
 
-	// далее в основном обработка событий, названия которых говорят сами за себя
-	cluster.on('cluster.ready', (pid) => {
-		ws.broadcast("worker.list", workers);
-	});
+  // если клиент подключился, скидываем ему актуальную информацию
+  ws.on('connection', (reply) => {
+    reply('worker.list', workers);
+    reply('cluster.info', info);
+  });
 
-	// если клиент подключился, скидываем ему актуальную информацию
-	ws.on("connection", (reply) => {
-		reply("worker.list", workers);
-		reply("cluster.info", info);
-	});
+  // иногда клиент может и сам попросить актуальную информацию
+  ws.on('worker.getfullinfo', (reply) => {
+    reply('worker.list', workers);
+    reply('cluster.info', info);
+  });
 
-	// иногда клиент может и сам попросить актуальную информацию
-	ws.on("worker.getfullinfo", (reply) => {
-		reply("worker.list", workers);
-		reply("cluster.info", info);
-	});
+  let isProcessTerminated = false;
+  // клиент хочет когото убить
+  ws.on('worker.kill', (reply, pid) => {
+    if (isProcessTerminated) {
+      reply('message', 'попробуйте убить процесс через несколько секунд');
+      return;
+    }
 
-	let isProcessTerminated = false;
-	// клиент хочет когото убить
-	ws.on("worker.kill", (reply, pid) => {
+    isProcessTerminated = true;
+    setTimeout(() => {
+      isProcessTerminated = false;
+    }, 10000);
 
-		if(isProcessTerminated){
-			reply('message', 'попробуйте убить процесс через несколько секунд');
-			return;
-		} 
+    const list = cluster.getWorkers();
+    if (list.length == 1) {
+      reply('message', 'Извините, но я последний боец в этом окопе!!!');
+      return;
+    }
 
-		isProcessTerminated=true;
-		setTimeout(()=>{
-			isProcessTerminated=false;
-		},10000);
+    const worker = cluster.getWorker(pid);
+    if (!worker) {
+      reply('message', 'воркер уже был мертв))))');
+      workers[pid].alive = false;
+      ws.broadcast('worker.list', workers);
+    } else if (pid === process.pid) {
+      reply('message', 'Эхх, подстрелили командира?');
 
-		const list = cluster.getWorkers();
-		if (list.length == 1) {
-			reply('message', 'Извините, но я последний боец в этом окопе!!!');
-			return;
-		}
+      // прекращаем генерацию данных
+      dataProcess.stop();
 
-		const worker = cluster.getWorker(pid);
-		if (!worker) {
-			reply('message', 'воркер уже был мертв))))');
-			workers[pid].alive = false;
-			ws.broadcast("worker.list", workers);
-		} else {
+      // выбираем процесс для передачи ему мАстерских полномочий
+      const nextMaster = list.filter((item) => item.pid != worker.pid)[0];
 
-			if (pid === process.pid) {
-				reply('message', 'Эхх, подстрелили командира?');
-				
-				// прекращаем генерацию данных
-				dataProcess.stop();
+      storeWorker(pid, { status: 'worker',	alive: false });
+      storeWorker(nextMaster.pid, { status: 'master',	alive: true });
 
-				// 	выбираем процесс для передачи ему мАстерских полномочий
-				nextMaster = list.filter((item) => item.pid != worker.pid)[0];
+      ws.broadcast('worker.list', workers);
 
-				storeWorker(pid, { status: "worker",	alive: false });
-				storeWorker(nextMaster.pid, { status: "master",	alive: true });
+      // выбранный стать мастером воркер говорит что получил инфу
+      cluster.on('task.isReady', () => {
+        ws.stop();
+        nextMaster.setAsMaster(pid);
+      });
 
-				ws.broadcast("worker.list", workers);
+      // выбранный стать мастером воркер говорит что уже стал им (прощается))
+      cluster.on('task.isMaster', () => {
+        // убиваем сами себя
+        worker.kill(null, process.pid);
+      });
 
-				// выбранный стать мастером воркер говорит что получил инфу
-				cluster.on("task.isReady", () => {
-					ws.stop();
-					nextMaster.setAsMaster(pid);
-				});
+      // перед смертью чтото делаем, притом синхронно (специфика выхода по process.exit() )
+      cluster.on('cluster.stop', (pid) => {
+        fs.write(fd, `kill from ${pid}\n`, () => {});
+        fs.closeSync(fd);
+        // process.exit();
+        // process.kill(process.pid);
+      });
 
-				// выбранный стать мастером воркер говорит что уже стал им (прощается))
-				cluster.on("task.isMaster", () => {
-					// убиваем сами себя
-					worker.kill(null,process.pid);
-				});
+      // перед тем как сделать выбранный воркер мастером отправляем ему текущую инфу
+      nextMaster.send('task.fullinfo', workers, info, pid);
+    } else {
+      workers[pid].alive = false;
+      worker.kill(null, process.pid);
+      ws.broadcast('worker.list', workers);
+    }
+  });
 
-				// перед смертью чтото делаем, притом синхронно (специфика выхода по process.exit() )
-				cluster.on("cluster.stop", (pid) => {
-					fs.write(fd, `kill from ${pid}\n`,()=>{});					
-					fs.closeSync(fd);
-					// process.exit();
-					// process.kill(process.pid);
-				});
+  // нет, хотя бы одного бойца надо оставить
+  ws.on('worker.killall', (reply) => {
+    reply('message', 'мы так не договаривались))))');
+  });
 
-				// перед тем как сделать выбранный воркер мастером отправляем ему текущую инфу
-				nextMaster.send("task.fullinfo", workers, info, pid);
+  let isProcessStarted = false;
+  // принимаем от интерфейса команду на создание дочернего процесса
+  ws.on('worker.fork', (reply) => {
+    console.log('fork 01');
 
-			} else {
-				workers[pid].alive = false;
-				worker.kill(null,process.pid);
-				ws.broadcast("worker.list", workers);
-			}
-		}
-	});
+    if (isProcessStarted) {
+      reply('message', 'попробуйте запустить процесс чуть позже');
+      return;
+    }
+    console.log('fork 02');
 
-	// 	нет, хотя бы одного бойца надо оставить
-	ws.on("worker.killall", (reply) => {
-		reply('message', 'мы так не договаривались))))');
+    isProcessStarted = true;
+    setTimeout(() => {
+      isProcessStarted = false;
+    }, 5000);
 
-	});
+    if (Object.values(workers).filter((worker) => worker.alive).length > 7) {
+      reply('message', 'пожалейте мой компьютер');
+      return;
+    }
+    console.log('fork 03');
 
+    const worker = cluster.fork();
+    // дочерний процесс готов (запустил IPC сервер, подключился к мастеру
+    // а мастер подключился к дочернему процессу)
+    worker.on('worker.ready', () => {
+      storeWorker(worker.pid);
+      ws.broadcast('worker.list', workers);
+      ws.broadcast('cluster.info', info);
+    });
+  });
 
-	let isProcessStarted = false;
-	// принимаем от интерфейса команду на создание дочернего процесса
-	ws.on("worker.fork", (reply) => {
-		console.log('fork 01');
-		
-		if(isProcessStarted){
-			reply('message', 'попробуйте запустить процесс чуть позже');
-			return;
-		} 
-		console.log('fork 02');
+  // даем время запустится web серверу
+  setTimeout(() => {
+    // 	генерируем и рассылаем данные
+    dataProcess.start(() => {
+      const number = Math.floor(Math.random() * 1000);
+      const pids = Object.values(workers)
+        .filter((worker) => worker.alive)
+        .map((worker) => worker.pid);
+      if (pids.length == 0) return;
+      const r = Math.floor(Math.random() * pids.length);
+      const pid = pids[r];
+      const worker = cluster.getWorker(pid);
 
-		isProcessStarted=true;
-		setTimeout(()=>{
-			isProcessStarted=false;
-		},5000);
+      if (!worker) return;
 
-		if (Object.values(workers).filter(worker=>worker.alive).length > 7) {
-			reply('message', 'пожалейте мой компьютер');
-			return;
-		}
-		console.log('fork 03');
+      info.generated++;
+      workers[process.pid].generated++;
 
-		const worker = cluster.fork();
-        // дочерний процесс готов (запустил IPC сервер, подключился к мастеру а мастер подключился к дочернему процессу)
-		worker.on("worker.ready", () => {
-			storeWorker(worker.pid);
-			ws.broadcast("worker.list", workers);
-			ws.broadcast("cluster.info", info);
-		});
-	});
+      // отправляем выбранному процессу данные на обработку
+      worker.send('process.data', process.pid, number);
 
-	// даем время запустится web серверу
-	setTimeout(()=>{
+      // пишем в лог сгенерированныечисла
+      fs.write(fd, `${number} generated for ${pid}\n`, () => {});
+      // log.generated[process.pid].push(number);
+    }, 500);
 
-		// 	генерируем и рассылаем данные
-		dataProcess.start(()=>{
-			const number = Math.floor(Math.random()*1000);
-			const pids = Object.values(workers).filter(worker=>worker.alive).map(worker=>worker.pid);
-			if(pids.length==0) return;
-			const r = Math.floor(Math.random()*pids.length);
-			const pid = pids[r];
-			const worker = cluster.getWorker(pid);
-
-			if(!worker) return;
-
-			info.generated++;
-			workers[process.pid].generated++;
-
-			// отправляем выбранному процессу данные на обработку 
-			worker.send("process.data", process.pid, number);
-	
-			// пишем в лог сгенерированныечисла
-			fs.write(fd, `${number} generated for ${pid}\n`,()=>{});
-			// log.generated[process.pid].push(number);
-		},500);
-
-		// 	кидаем инфу браузерным клиентам
-		dataProcess.start(()=>{
-			ws.broadcast("worker.list", workers);
-			ws.broadcast("cluster.info", info);		
-		},500);
-
-	},5000);
-
+    // кидаем инфу браузерным клиентам
+    dataProcess.start(() => {
+      ws.broadcast('worker.list', workers);
+      ws.broadcast('cluster.info', info);
+    }, 500);
+  }, 5000);
 }
 
 // эти обработчики предназначены для обработки данных и висят вне зависимости от мастер/воркер
 // т.е. данные могут обрабатывать и мастер и воркеры
 
 // если мы генерировали данные нам и учитывать их)
-cluster.on("process.data.ready", (pid, number)=>{
-	info.processed++;
-	workers[pid].processed++;
+cluster.on('process.data.ready', (pid, number) => {
+  info.processed++;
+  workers[pid].processed++;
 });
 
 // обрабатываем входящие данные (пишем в файл лога)
-cluster.on("process.data", (pid, number)=>{
-	// пишем в лог обработанные ечисла
-	fs.write(fd, `${number} from ${pid}\n`,()=>{});
-	//log.processed.push(number);
+cluster.on('process.data', (pid, number) => {
+  // пишем в лог обработанные ечисла
+  fs.write(fd, `${number} from ${pid}\n`, () => {});
+  // log.processed.push(number);
 
-	// подтверждаем обработку
-	cluster.send(pid,"process.data.ready", process.pid, number);
+  // подтверждаем обработку
+  cluster.send(pid, 'process.data.ready', process.pid, number);
 });
--- modules/storage/storage.js
+++ modules/storage/storage.js
@@ -1,14 +1,21 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+/* eslint-disable no-console */
 /**
- * Модуль реализует автоматическое сохранение данных при выходе из программы (завершении процесса) и их восстановление при повторном запуске
- * 
+ * Модуль реализует автоматическое сохранение данных при выходе из программы (завершении процесса)
+ * и их восстановление при повторном запуске
+ *
  * Архитектура данного модуля реализует следующие принципы SOLID:
- * - [S] Принцип единственной ответственности (Модуль, как самостоятельный элемент сравнимый с классом инкапсулирует все ресурсы необходимые для своей работы)
+ * - [S] Принцип единственной ответственности (Модуль, как самостоятельный элемент сравнимый
+ *       с классом инкапсулирует все ресурсы необходимые для своей работы)
  * - [O] Принцип открытости/закрытости (достигается за счет унификации API и использования defaults)
- * - [L] Принцип подстановки Лисков (достигается за счет того, что все реализации наследуются от прототипа DummyStorage и следуют соглашению/рекомендациям
- *   	 по переопределению методов класса DummyStorage)
+ * - [L] Принцип подстановки Лисков (достигается за счет того, что все реализации
+ *       наследуются от прототипа DummyStorage и следуют соглашению/рекомендациям
+ *       по переопределению методов класса DummyStorage)
  * - [I] Принцип разделения интерфейса (тут очевидно...)
- * - [D] Принцип инверсии зависимостей (Достигается за счет того, что класс DummyStorage по сути является абстракцией, не зависящей от реализаций)
- * 
+ * - [D] Принцип инверсии зависимостей (Достигается за счет того, что класс DummyStorage
+ *       по сути является абстракцией, не зависящей от реализаций)
+ *
  * @example
  * const storage = Storage();
  * const test = storage.create('test',[],true);
@@ -16,7 +23,7 @@
  *                    // ['eniItem'] - при втором запуске приложения
  *                    // ['eniItem','eniItem'] - при третьем запуске приложения
  * test.push('eniItem');
- * 
+ *
  * @example
  * const storage = Storage();
  * const test = storage.create('test',{},true);
@@ -24,40 +31,38 @@
  *                    // {'1000': 'eniItem'} - при втором запуске приложения
  *                    // {'1000': 'eniItem', '1001': 'eniItem'} - при третьем запуске приложения
  * test[process.pid] = 'eniItem';
- * 
+ *
  * @module modules/storage
- * 
+ *
  * @author Roman Surmanidze <lastuniverse@github.com>
  */
 
 // используемая по умолчанию реализация хранения и восстановления данных
 const defaults = {
-	type: 'file',
-	options: __dirname+'/store'
+  type: 'file',
+  options: `${__dirname}/store`,
 };
 
-
 /**
- * Загружает класс-реализацию механизмов фонового сохранения и загрузки данных и создает объект от класса реализации
- * 
+ * Загружает класс-реализацию механизмов фонового сохранения
+ * и загрузки данных и создает объект от класса реализации
+ *
  * @class      Storage (name)
- * @param      {array}                  options  Набор произвольных параметров, передаваемых конструктору реализации
- *                                               Каждая реализация обладает своим набором параметров.
+ * @param      {array}                  options  Набор произвольных параметров,
+ *                                               передаваемых конструктору реализации
+ *                                               Каждая реализация обладает
+ *                                               своим набором параметров.
  * @return     {StorageImplementation}  объект класса-реализации
  */
 function Storage(...options) {
-    try {
-        StorageImplementation = require(`./implementations/${defaults.type}-storage.js`);
-    } catch (e) {
-    	console.log(e);
-        return console.warn(`Do not find '${defaults.type}' implementation of Storage`);
-    }
-
+  try {
+    const StorageImplementation = require(`./implementations/${defaults.type}-storage.js`);
     const storrage = new StorageImplementation(...options);
     return storrage;
+  } catch (e) {
+    console.log(e);
+    return console.warn(`Do not find '${defaults.type}' implementation of Storage`);
+  }
 }
 
-
 exports = module.exports = Storage;
-
-
