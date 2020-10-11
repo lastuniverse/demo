dbbb6dcffdb69519e2ae3f2d3af130389d60c252
lastuniverse <lastuniverse@mail.ru>
2020-10-12 01:23:04 +0200

    test

--- git.push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git add .
-
-git commit -m "autopush: $*"
-
-git push -u origin master
--- kill.node.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ps -ef | grep '.js' | grep -v grep | awk '{print $2}' | xargs -r kill -9
